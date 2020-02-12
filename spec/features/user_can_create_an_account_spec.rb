148dad3262422a1ad851c5318415e0b256482cf6
Blake <blake@futchi.com>
2020-02-12 17:29:43 +0100

    test created for invalid email

--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -1,20 +1,45 @@
 require "rails_helper"
 
-feature 'user can create a new account' do
-  before do
-    visit new_user_registration_path
-  end
-
-  feature 'happy path' do
- 
-    before do
-      fill_in 'Name', with: 'Blake'
-      fill_in 'Email', with: 'email@email.com'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-
-      click_on 'Create'
+feature 'User can create a new account' do
+    before do 
+        visit new_user_registration_path
     end
+
+    describe 'successfully' do
+        before do             
+            fill_in 'Name', with: 'Blake'
+            fill_in 'Email', with: 'example@email.com'
+            fill_in 'Password', with: 'password'
+            fill_in 'Password confirmation', with: 'password'
+            click_on 'Create'
+        end
+
+        it 'displays welcome message' do
+            expect(page).to have_content 'Hello, Blake'
+        end
+
+        it 'redirects to root path' do
+            expect(current_path).to eq root_path
+        end
+    end
+
+    describe 'unsuccessfully' do
+        describe 'with invalid email' do
+            before do
+                fill_in 'Name', with: 'Blake'
+                fill_in 'Email', with: 'email.com'
+                fill_in 'Password', with: 'pass'
+                fill_in 'Password confirmation', with: 'pass'
+                click_on 'Create'
+            end
+
+            it 'returns error message' do
+                expect(page).to have_content 'Email is invalid'
+            end
+
+        end
+    end
+end
 
     # redundant tests
     # it 'displays name field' do
@@ -32,15 +57,4 @@ feature 'user can create a new account' do
     # it 'displays  password confirmation' do
     #   expect(page).to have_field 'Password confirmation'
     # end
-
-    it 'displays welcome message' do
-      expect(page).to have_content 'Hello, Blake'
-    end
-
-    it 'redirects to root path' do
-      expect(current_path).to eq root_path
-    end
-    
-  end
-end
 
