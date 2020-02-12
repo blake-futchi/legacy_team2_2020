0ebbd01eeee94226ea4e81626764e7cad6de6158
Blake <blake@futchi.com>
2020-02-12 16:45:33 +0100

    redirects to logged in and receives welcome message

--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -1,25 +1,46 @@
 require "rails_helper"
 
-feature 'List registration form fields' do
-  context 'with articles in db' do
+feature 'user can create a new account' do
+  before do
+    visit new_user_registration_path
+  end
+
+  feature 'happy path' do
+ 
     before do
-      visit new_user_registration_path
+      fill_in 'Name', with: 'Blake'
+      fill_in 'Email', with: 'email@email.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+
+      click_on 'Create'
     end
 
-    it 'displays name field' do
-      expect(page).to have_field 'Name'
+    # redundant tests
+    # it 'displays name field' do
+    #   expect(page).to have_field 'Name'
+    # end
+
+    # it 'displays email field' do
+    #   expect(page).to have_field 'Email'
+    # end
+
+    # it 'displays password field' do
+    #   expect(page).to have_field 'Password'
+    # end
+
+    # it 'displays  password confirmation' do
+    #   expect(page).to have_field 'Password confirmation'
+    # end
+
+    it 'displays welcome message' do
+      expect(page).to have_content 'Hello, Blake'
     end
 
-    it 'displays email field' do
-      expect(page).to have_field 'Email'
+    it 'redirects to root path' do
+      expect(current_path).to eq root_path
     end
-
-    it 'displays password field' do
-      expect(page).to have_field 'Password'
-    end
-
-    it 'displays  password confirmation' do
-      expect(page).to have_field 'Password confirmation'
-    end
+    
   end
 end
+
