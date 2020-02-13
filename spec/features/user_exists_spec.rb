127923f9263017f2bc37e6745b39f5c4fb051d16
Blake <blake@futchi.com>
2020-02-13 15:50:21 +0100

    able to call factorybot and pass user signup for existing user account

--- spec/features/user_exists_spec.rb
+++ spec/features/user_exists_spec.rb
@@ -1,9 +1,20 @@
 require "rails_helper"
 
+feature 'User already has account' do
 
+  before do
+    visit new_user_registration_path
+  end
   
-describe 'Factory' do
-  it 'should have valid Factory' do
-    expect(FactoryBot.create(:user)).to be_valid
-  end  
+
+  describe 'account exists error' do
+    let(:registered_user) {create(:user)} #calls factorybot to create our friend Pierre
+
+  it 'expect error message' do
+    fill_in 'Email', with: registered_user.email
+    fill_in 'Password', with: 'password'
+    click_on 'Create'
+    expect(page). to have_content 'Email has already been taken'
+  end
+  end
 end
