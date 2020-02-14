4d679900a2e407b5e44b065e6f6086335f513767
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-14 13:38:37 +0100

    Adds spec for compose button accessed from welcome_index

--- spec/features/user_can_create_a_conversation_spec.rb
+++ spec/features/user_can_create_a_conversation_spec.rb
@@ -1,44 +1,34 @@
-#on click compose user is directed to new path : form.html.erb
 require "rails_helper"
 
 feature 'User can create a new message' do
+    let(:user) { FactoryBot.create(:user, email:'user1_email@gmail.com', password:'password1' )}
     before do 
-        visit welcome_index_path
-    end
+        visit root_path 
+        click_on 'Login'
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_on 'Log in'
+        click_on 'Inbox'
+        click_on 'Compose'
+    end 
 
-    describe 'create new message' do
-        
-        before do
-            click_button 'Compose'
+    describe 'user can view the compose new message fields' do 
+
+        it 'link to new conversation' do
+        expect(current_path).to eq new_conversation_path
         end
 
         it 'displays recipients field' do
-          expect(page).to have_content 'Recipients'
+        expect(page).to have_content 'Recipients'
         end
+
         it 'displays subject field' do
-            expect(page).to have_content 'Subject'
+        expect(page).to have_content 'Subject'
         end
+
         it 'displays message field' do
-            expect(page).to have_content 'Message'
+        expect(page).to have_content 'Message'
         end
 
-    end
-
+    end 
 end
-
-    # redundant tests
-    # it 'displays name field' do
-    #   expect(page).to have_field 'Name'
-    # end
-
-    # it 'displays email field' do
-    #   expect(page).to have_field 'Email'
-    # end
-
-    # it 'displays password field' do
-    #   expect(page).to have_field 'Password'
-    # end
-
-    # it 'displays  password confirmation' do
-    #   expect(page).to have_field 'Password confirmation'
-    # end
